32d014144f445cab1937a90e4351c9d1e3074ddb
bububan.github.io <3361456053@qq.com>
2026-03-20 15:18:37 +0800

    Site updated: 2026-03-20 15:18:36

--- js/cursor.js
+++ js/cursor.js
@@ -167,3 +167,24 @@ class Cursor {
   CURSOR = new Cursor();
   // 需要重新获取列表时，使用 CURSOR.refresh()
 })();
+
+
+/* 恶搞标题 start */
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    //离开当前页面时标签显示内容
+    document.title = '👀跑哪里去了~';
+    clearTimeout(titleTime);
+  } else {
+    //返回当前页面时标签显示内容
+    document.title = '🐖抓到你啦～';
+    //两秒后变回正常标题
+    titleTime = setTimeout(function () {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
+/* 恶搞标题 end */
