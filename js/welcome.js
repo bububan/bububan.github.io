8983bdc5ec0fd433de7ecf7b5f9466e97053e8f7
bububan.github.io <3361456053@qq.com>
2026-03-25 15:18:01 +0800

    Site updated: 2026-03-25 15:18:01

--- js/welcome.js
+++ js/welcome.js
@@ -1,24 +1,24 @@
 fetch('https://ipapi.co/json/')
-  .then(res => res.json())
-  .then(data => {
+.then(res => res.json())
+.then(data => {
 
-    const city = data.city
-    const region = data.region
-    const ip = data.ip
+  const city = data.city
+  const region = data.region
+  const ip = data.ip
 
-    const welcome = document.getElementById("welcome-info")
+  const welcome = document.getElementById("welcome-info")
 
-    let time = new Date().getHours()
-    let greeting = ""
+  let hour = new Date().getHours()
+  let greeting = ""
 
-    if (time < 11) greeting = "早上好"
-    else if (time < 13) greeting = "中午好"
-    else if (time < 18) greeting = "下午好"
-    else greeting = "晚上好"
+  if (hour < 11) greeting = "早上好"
+  else if (hour < 13) greeting = "中午好"
+  else if (hour < 18) greeting = "下午好"
+  else greeting = "晚上好"
 
-    welcome.innerHTML = `
-    🎉 欢迎信息 🎉<br>
-    欢迎来自 <b>${region} ${city}</b> 的伙伴，${greeting}！<br>
-    您的IP地址为：<b>${ip}</b>
-    `
-  })
+  welcome.innerHTML = `
+  🎉 欢迎信息 🎉<br>
+  欢迎来自 ${region} ${city} 的伙伴，${greeting}<br>
+  您的IP地址是：${ip}
+  `
+})
