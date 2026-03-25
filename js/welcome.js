e483d0b667016f279ce5f513b3dfb98b52fd4a45
bububan.github.io <3361456053@qq.com>
2026-03-25 15:46:52 +0800

    Site updated: 2026-03-25 15:46:51

--- js/welcome.js
+++ js/welcome.js
@@ -5,10 +5,36 @@ fetch('https://ipapi.co/json/')
   const city = data.city
   const region = data.region
   const ip = data.ip
+  const lat = data.latitude
+  const lon = data.longitude
+
+  // 站长位置（东莞）
+  const blogLat = 23.02067
+  const blogLon = 113.75179
+
+  function getDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371
+    const dLat = (lat2 - lat1) * Math.PI / 180
+    const dLon = (lon2 - lon1) * Math.PI / 180
+
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1 * Math.PI / 180) *
+      Math.cos(lat2 * Math.PI / 180) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2)
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+    return Math.round(R * c)
+  }
+
+  const distance = getDistance(lat, lon, blogLat, blogLon)
 
   const welcome = document.getElementById("welcome-info")
 
-  let hour = new Date().getHours()
+  const hour = new Date().getHours()
+
   let greeting = ""
 
   if (hour < 11) greeting = "早上好"
@@ -18,7 +44,8 @@ fetch('https://ipapi.co/json/')
 
   welcome.innerHTML = `
   🎉 欢迎信息 🎉<br>
-  欢迎来自 ${region} ${city} 的伙伴，${greeting}<br>
-  您的IP地址是：${ip}
+  欢迎来自 <b>${region} ${city}</b> 的伙伴，${greeting}<br>
+  距离站长 <b>${distance}</b> 公里<br>
+  当前IP地址为：<b>${ip}</b>
   `
 })
