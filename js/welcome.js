b9e9d269f5c65dbc69b69b42c509f405a6a503db
bububan.github.io <3361456053@qq.com>
2026-03-27 10:28:33 +0800

    Site updated: 2026-03-27 10:28:33

--- js/welcome.js
+++ js/welcome.js
@@ -2,8 +2,14 @@ fetch("https://api.vvhan.com/api/ipInfo")
 .then(res => res.json())
 .then(data => {
   const city = data.info.city
+  const ip = data.ip
+
   const welcome = document.getElementById("welcome-info")
+
   if(welcome){
-    welcome.innerHTML = "欢迎来自 " + city + " 的小伙伴"
+    welcome.innerHTML =
+    `欢迎来自 <b>${city}</b> 的小伙伴，
+    祝你在本站玩得开心！<br>
+    当前IP地址：${ip}`
   }
 })
